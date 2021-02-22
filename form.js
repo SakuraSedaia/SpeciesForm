2453f198c6b91803932c74c1051dfb2b802132d6
SakuraSedaia <61118816+SakuraSedaia@users.noreply.github.com>
2021-02-22 17:11:31 -0600

    Started changing over to Dropdown Lists

--- form.js
+++ form.js
@@ -5,24 +5,33 @@ var extraPos = document.getElementById('extraPos')
 // Species Selectors
 var extraHeight = '1.4em'
 var returnSpeciesValue
-// Which list should I show?
-function list () {
-  returnSpeciesValue = document.getElementById('speciesSelect').value
 
-  if (returnSpeciesValue == 'Human') {
-    extraContain.style.height = extraHeight
-    console.log('Human')
-    extraPos.style.transform = 'translateY(0em)'
-  } else if (returnSpeciesValue == 'Ardoni') {
-    extraContain.style.height = extraHeight
-    console.log('Ardoni')
-    extraPos.style.transform = 'translateY(-25px)'
-  } else {
-    extraContain.style.height = '0em'
-    console.log(null)
-  }
+var hybrid = document.getElementById('hybrid')
+var hLab = document.getElementById('hybridLabel')
+var hBox = document.getElementById('hybridToggle')
+var maxAge
+// Which list should I show?
+function list() {
+    returnSpeciesValue = document.getElementById('speciesSelectList').value
+    console.log("Fuck You")
+
+    if (returnSpeciesValue == 'Human') {
+    } 
+    
+    
+    if (returnSpeciesValue == 'Magnorite') {
+        hybrid.style.visibility = "hidden"
+    } else {
+        hybrid.style.visibility = "initial"
+        hBox.checked = false;
+    }
 }
 
-function submit() {
-
+function updateHybrid() {
+    if (hBox.checked == true) {
+        hBox.checked = false
+        console.log('I fucking hate you')
+    } else {
+        hBox.checked = true
+    }
 }
