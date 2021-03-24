f2c07e5d509e3dc9421822e9d5e98b28a8218214
SakuraSedaia <61118816+SakuraSedaia@users.noreply.github.com>
2021-03-23 19:19:27 -0500

    Started Rewrite of this shit

--- brainfuck.js
+++ brainfuck.js
@@ -1,195 +1,87 @@
-// Elements
-var extraContain = document.getElementById('extras')
-var extraPos = document.getElementById('extraPos')
+// Default Values
+var defaultGender = "Male";
+var defaultSpecies = "Human";
+var defaultSecondSpecies = "None";
+var maxAges = [
+  "100", // Human
+  "100", // Felina
+  "300", // Ardoni
+  "800", // Netheran
+  "1000", // Glacian
+  "100", // Jagathan
+  "30", // Magnorite
+];
+var defaultHumanClass = "None";
+var defaultArdoniClan = "Clanless";
+var hybridOnByDefault = false;
+var necroOnByDefault = false;
 
-var extraContainTwo = document.getElementById('extrasTwo')
-var extraPosTwo = document.getElementById('extraPosTwo')
+// AUTOMATED SCRIPT PROCESS, DO NOT EDIT
+console.log("Script Information:\n> SoW OC Form Script\n> Created by: Sakura Sedaia\n> Version: V1.1 (Script Rewrite)\n=====================\n");
+console.group("Importing Hardcoded HTML Elements to Script");
+var grabMainSpecies = document.getElementById("speciesSelect");
+var mainSpeciesID = [];
+console.log("Pulled MainSpecies Select from HTML");
 
-// Species Selectors
-var returnSpeciesValue = document.getElementById('speciesSelectList').value
-var currentID =
-  returnSpeciesValue
-    .substr(0, 3)
-    .toString()
-    .toLowerCase() + 'CB'
-var hybrid = document.getElementById('hybrid')
-var hLab = document.getElementById('hybridLabel')
-var hBox = document.getElementById('hybridToggle')
-var crossContain = document.getElementById('crossbreed')
-var secondSpeciesList = document.getElementById('speciesSelectListTwo')
+var grabSecondSpecies = document.getElementById("crossbreed");
+var secondSpeciesID = [];
+console.log("Pulled SecondSpecies Select from HTML");
 
-// Miscellaneous Variables
-var maxAge
-var speciesCount = secondSpeciesList.childElementCount
-var disabledSpecies = []
+var extras = document.getElementById("extras");
+console.log("Pulled Extras Container (Main) from HTML");
 
-// Modify form based on Species Selection
-function speciesSelect () {
-  returnSpeciesValue = document.getElementById('speciesSelectList').value
-  currentID =
-    returnSpeciesValue
-      .substr(0, 3)
-      .toString()
-      .toLowerCase() + 'CB'
+var extraPos = document.getElementById("extraPos");
+console.log("Pulled Extras Position (Main) from HTML");
 
-  var extraHeight = '1.4em'
-  // Human/Ardoni Clan/Class Tags
-  if (returnSpeciesValue == 'Human' || returnSpeciesValue == 'Ardoni') {
-    // Apply Height
-    extraContain.style.height = extraHeight
-  }
-  if (returnSpeciesValue == 'Human') {
-    // Apply Human Ruleset
-    extraPos.style.transform = 'translateY(0px)'
-  } else if (returnSpeciesValue == 'Ardoni') {
-    // Apply Ardoni Rulesets
-    extraPos.style.transform = 'translateY(-25px)'
-  } else {
-    extraContain.style.height = '0em'
-    extraPos.style.transform = 'translateY(0px)'
-  }
-  createSpeciesFilter()
-  console.log('Species set to "' + returnSpeciesValue + '"')
+var humExt = document.getElementById("humExt");
+console.log("Pulled Human Extras (Main) from HTML");
+
+var ardoClan = document.getElementById("ardoClan");
+console.log("Pulled Ardoni Clans (Main) from HTML");
+
+console.groupEnd("Importing Hardcoded HTML Elements to Script");
+console.log("=====================");
+console.group("Create Primary Species Element Variables");
+
+var i = 0;
+while (i < 7) {
+  var grabValues = grabMainSpecies.getElementsByTagName("option")[i].innerText.toLowerCase().substring(0, 3);
+  var currentID = grabValues + "Main";
+  grabMainSpecies.getElementsByTagName("option")[i].setAttribute("id", currentID);
+  mainSpeciesID.push(document.getElementById(currentID));
+  console.log("ID Created: " + currentID);
+  i++;
 }
+console.groupEnd("Create Primary Species Element Variables");
 
-// Modify CB form based on Species Selection
-function speciesSelectTwo () {
-  returnSpeciesValueTwo = document.getElementById('speciesSelectListTwo').value
-
-  var extraHeightTwo = '1.4em'
-  // Human/Ardoni Clan/Class Tags
-  if (returnSpeciesValueTwo == 'Human' || returnSpeciesValueTwo == 'Ardoni') {
-    // Apply Height
-    extraContainTwo.style.height = extraHeightTwo
-  }
-  if (returnSpeciesValueTwo == 'Human') {
-    // Apply Human Ruleset
-    extraPosTwo.style.transform = 'translateY(0px)'
-  } else if (returnSpeciesValueTwo == 'Ardoni') {
-    // Apply Ardoni Rulesets
-    extraPosTwo.style.transform = 'translateY(-25px)'
-  } else {
-    // Revert Ardoni/Human Rulesets
-    extraContainTwo.style.height = '0em'
-    extraPosTwo.style.transform = 'translateY(0px)'
-  }
-  console.log('Species set to "' + returnSpeciesValueTwo + '"')
+console.group("Create Secondary Species Element Variables");
+var i = 1;
+while (i < 8) {
+  var grabValues = grabSecondSpecies.getElementsByTagName("option")[i].innerText.toLowerCase().substring(0, 3);
+  var currentID = grabValues + "Second";
+  grabSecondSpecies.getElementsByTagName("option")[i].setAttribute("id", currentID);
+  secondSpeciesID.push(document.getElementById(currentID));
+  console.log("ID Created: " + currentID);
+  i++;
 }
+console.groupEnd("Create Secondary Species Element Variables");
+console.log("=====================");
+var defaultConsole = console.group("Set Default Values");
 
-function updateHybrid () {
-  if (hBox.checked == true) {
-    hBox.checked = false // Sets the Form Value to "False"
-
-    console.info('Crossbreed Disabled')
-    // Style the Hybrid Toggle Button
-    hLab.style.color = 'black'
-
-    // Hide CB Options and reset them
-    crossContain.style.height = '0px'
-  } else {
-    hBox.checked = true // Sets the Form Value to "True"
-
-    console.info('Crossbreed Enabled')
-    // Style the Hybrid Toggle Button
-    hLab.style.color = 'red'
-
-    // Show CB Options
-    crossContain.style.height = 'auto'
-  }
-  createSpeciesFilter()
+console.log('Default Gender Set to "' + defaultGender + '"\n');
+console.log('Default Primary Species Set to "' + defaultSpecies + '"\n');
+console.log('Default Secondary Species Set to "' + defaultSecondSpecies + '"\n');
+var ageConsole = console.group("Default Ages");
+var i = 0;
+while (i < maxAges.length) {
+  console.log(mainSpeciesID[i].innerText.toString() + " Max age set to " + maxAges[i]);
+  i++;
 }
-
-var mainList = document
-  .getElementById('speciesSelectList')
-  .getElementsByTagName('option')
-
-// Crossbreed Species Filters
-function createSpeciesFilter () {
-  console.group('Filter Update')
-
-  // Get List Elements
-  var cbList = secondSpeciesList
-  var listSubElms = cbList.getElementsByTagName('option')
-
-  disabledSpecies.splice(0, disabledSpecies.length) // Reset Array
-
-  disabledSpecies.push(currentID)
-
-  // Apply Filters
-  if (returnSpeciesValue == 'Netheran') {
-    disabledSpecies.push('glaCB') // Remove Glacian from Netheran CB Options
-  } else if (returnSpeciesValue == 'Glacian') {
-    disabledSpecies.push('netCB') // Remove Netheran from Glacian CB Options
-  } else if (returnSpeciesValue == 'Magnorite') {
-    // Remove everything from form when Magnorite is selected
-    for (var i = 0; i < mainList.length - 1; i++) {
-      disabledSpecies.push(
-        mainList[i].innerText
-          .substr(0, 3)
-          .toString()
-          .toLowerCase() + 'CB'
-      )
-    }
-
-    // Disable and Hide Crossbreed Section
-    hybrid.style.opacity = '0%'
-    hybrid.style.pointerEvents = 'none'
-    hBox.checked = false
-    hBox.style.pointerEvents = 'none'
-    console.info('Crossbreed Disabled')
-    hLab.style.color = 'black'
-    crossContain.style.height = '0px'
-  } else {
-    // Invert Rulesets when Species is changed off of "Magnorite"
-
-    hybrid.style.opacity = '100%'
-    hybrid.style.pointerEvents = 'auto'
-    hBox.style.pointerEvents = 'auto'
-  }
-  console.info('Crossbreed Filter Updated')
-  console.log(disabledSpecies)
-
-  // Reset Filter
-  for (var i = 0; i < speciesCount; i++) {
-    // Total of 7 Elements
-    listSubElms[i].removeAttribute('disabled', '')
-    listSubElms[i].style.display = 'initial'
-  }
-  console.warn('Crossbreed Filter Reset')
-
-  // Disable Elements
-  for (var i = 0; i < disabledSpecies.length; i++) {
-    document
-      .getElementById(disabledSpecies[i].toString())
-      .setAttribute('disabled', '')
-    document
-      .getElementById(disabledSpecies[i].toString())
-      .setAttribute('hidden', '')
-  }
-  console.info('Filter Applied')
-  console.groupEnd('Filter Update')
-}
-
-// Necromancer Stuff
-var nLab = document.getElementById('necroButton')
-var nBox = document.getElementById('necroToggle')
-
-function updateNecro () {
-  if (nBox.checked == true) {
-    nBox.checked = false // Sets the Form Value to "False"
-
-    console.info('Necromancer Disabled')
-    // Style the Hybrid Toggle Button
-    nLab.style.color = 'black'
-  } else {
-    nBox.checked = true // Sets the Form Value to "True"
-
-    console.info('Necromancer Enabled')
-    nLab.style.color = 'red'
-  }
-}
-
-// Run Filters once to set Default Values
-speciesSelect()
-speciesSelectTwo()
-createSpeciesFilter()
+console.groupEnd(ageConsole);
+console.log('Default Human Class Set to "' + defaultHumanClass + '"\n');
+console.log('Default Ardoni Clan Set to "' + defaultArdoniClan + '"\n');
+console.log('Default Hybdrid State Set to "' + hybridOnByDefault + '"\n');
+console.log('Default Necromancer State Set to "' + necroOnByDefault + '"\n');
+console.groupEnd(defaultConsole);
+console.log("=====================\nSetup complete\n=====================\n");
+// ALL RESPONSIVE SCRIPT GOES UNDER HERE
